b23fd71bf156bcc1ebd1386a64dd46b5ef6323c9
Nhatdien <nhatkhonvl@gmail.com>
2025-06-02 17:02:23 +0700

    remove some not used store

--- stores/user_self_guilde_activity/index.ts
+++
@@ -1,6 +0,0 @@
-import { Base } from "../base";
-import { UserSelfGuideActivity, UserSelfGuideActivityResponse } from "~/types/user_self_guide_activity";
-
-export class UserSelfGuideActivities extends Base {
-
-}
